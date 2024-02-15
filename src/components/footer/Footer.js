e304e87f85ff8b21604acf122563940c9f1c6cd7
Mohit Bishnoi <148950241+nightcrypt@users.noreply.github.com>
2024-02-15 13:09:54 +0530

    contact us page of website

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>Copyright © 2019 All Rights Reserved By Free Html Templates</p>
+      
+    </div>
+  )
+}
+
+export default Footer
